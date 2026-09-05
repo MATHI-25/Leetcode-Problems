da1ae8fbcb2eec632dc65e7f02ab985f298eed61
Srimathi A <165049600+MATHI-25@users.noreply.github.com>
2026-09-05 14:42:08 +0530

    Time: 0 ms (100%), Space: 46.5 MB (73.88%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public int rob(TreeNode root) {
+
+        int[] result = solve(root);
+
+        return Math.max(result[0], result[1]);
+    }
+
+    public int[] solve(TreeNode node) {
+
+        if (node == null) {
+            return new int[]{0, 0};
+        }
+
+        int[] left = solve(node.left);
+        int[] right = solve(node.right);
+
+        // Rob the current node
+        int rob = node.val + left[1] + right[1];
+
+        // Don't rob the current node
+        int skip = Math.max(left[0], left[1])
+                 + Math.max(right[0], right[1]);
+
+        return new int[]{rob, skip};
+    }
+}
